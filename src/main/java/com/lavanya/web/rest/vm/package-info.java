947364ebf031873e3269ea906c5ba7ed3f4648b5
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-04-23 14:59:56 -0400

    Initial version of passion generated by generator-jhipster@8.3.0

---
+++ src/main/java/com/lavanya/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer visual models.
+ */
+package com.lavanya.web.rest.vm;
---
+++ src/test/java/com/lavanya/IntegrationTest.java
@@ -0,0 +1,20 @@
+package com.lavanya;
+
+import com.lavanya.config.AsyncSyncConfiguration;
+import com.lavanya.config.EmbeddedSQL;
+import com.lavanya.config.JacksonConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { PassionApp.class, JacksonConfiguration.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+public @interface IntegrationTest {
+}
